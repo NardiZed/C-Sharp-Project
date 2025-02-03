86739ea5d570848125e40b8258a3fb8369f6c1ca
Nardos Zerihun <nardoszerihun437@gmail.com>
2025-02-03 21:31:17 +0300

    Added a splash screen (Loading Page)

---
+++ Splash Screen.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace Final_Project
+{
+    partial class Splash_Screen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            components = new System.ComponentModel.Container();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            Panel1 = new Guna.UI2.WinForms.Guna2Panel();
+            Panel2 = new Guna.UI2.WinForms.Guna2Panel();
+            timer1 = new System.Windows.Forms.Timer(components);
+            SuspendLayout();
+            // 
+            // guna2HtmlLabel1
+            // 
+            guna2HtmlLabel1.BackColor = Color.Transparent;
+            guna2HtmlLabel1.Font = new Font("Verdana", 36F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2HtmlLabel1.ForeColor = Color.White;
+            guna2HtmlLabel1.Location = new Point(74, 184);
+            guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            guna2HtmlLabel1.Size = new Size(695, 88);
+            guna2HtmlLabel1.TabIndex = 0;
+            guna2HtmlLabel1.Text = "E-Commerce App";
+            // 
+            // guna2HtmlLabel2
+            // 
+            guna2HtmlLabel2.BackColor = Color.Transparent;
+            guna2HtmlLabel2.Font = new Font("Segoe UI Semibold", 16F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            guna2HtmlLabel2.ForeColor = Color.Silver;
+            guna2HtmlLabel2.Location = new Point(323, 495);
+            guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            guna2HtmlLabel2.Size = new Size(167, 47);
+            guna2HtmlLabel2.TabIndex = 1;
+            guna2HtmlLabel2.Text = "LOADING...";
+            // 
+            // Panel1
+            // 
+            Panel1.CustomizableEdges = customizableEdges1;
+            Panel1.Location = new Point(-2, 553);
+            Panel1.Name = "Panel1";
+            Panel1.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            Panel1.Size = new Size(849, 34);
+            Panel1.TabIndex = 2;
+            // 
+            // Panel2
+            // 
+            Panel2.BackColor = Color.Lime;
+            Panel2.CustomizableEdges = customizableEdges3;
+            Panel2.ForeColor = SystemColors.ControlLight;
+            Panel2.Location = new Point(-2, 554);
+            Panel2.Name = "Panel2";
+            Panel2.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            Panel2.Size = new Size(93, 34);
+            Panel2.TabIndex = 3;
+            // 
+            // timer1
+            // 
+            timer1.Enabled = true;
+            timer1.Interval = 10;
+            timer1.Tick += timer1_Tick;
+            // 
+            // Splash_Screen
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.Black;
+            ClientSize = new Size(844, 586);
+            Controls.Add(Panel2);
+            Controls.Add(Panel1);
+            Controls.Add(guna2HtmlLabel2);
+            Controls.Add(guna2HtmlLabel1);
+            FormBorderStyle = FormBorderStyle.None;
+            Name = "Splash_Screen";
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Splash_Screen";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2Panel Panel1;
+        private Guna.UI2.WinForms.Guna2Panel Panel2;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
---
+++ Splash Screen.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Final_Project
+{
+    public partial class Splash_Screen : Form
+    {
+        public Splash_Screen()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            
+            Panel2.Width += 3;
+            if(Panel2.Width >= 895)
+            {
+                timer1.Stop();
+                this.Hide();
+                Form1 form1 = new Form1();
+                form1.ShowDialog();
+                this.Dispose();
+            }
+        }
+    }
+}
