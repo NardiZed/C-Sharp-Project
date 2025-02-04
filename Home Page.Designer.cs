108d2d81de9aa312f5784869b878d561d581cc9c
Nardos Zerihun <nardoszerihun437@gmail.com>
2025-02-04 21:09:24 +0300

    Used a list box to display the name of the products from the database

--- Home Page.Designer.cs
+++ Home Page.Designer.cs
@@ -28,17 +28,19 @@
         /// </summary>
         private void InitializeComponent()
         {
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges5 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges6 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges1 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges2 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges3 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
-            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges4 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges11 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges12 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges7 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges8 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges9 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
+            Guna.UI2.WinForms.Suite.CustomizableEdges customizableEdges10 = new Guna.UI2.WinForms.Suite.CustomizableEdges();
             guna2Panel1 = new Guna.UI2.WinForms.Guna2Panel();
             guna2ControlBox2 = new Guna.UI2.WinForms.Guna2ControlBox();
             guna2ControlBox1 = new Guna.UI2.WinForms.Guna2ControlBox();
-            flowLayoutPanel1 = new FlowLayoutPanel();
+            panel1 = new Panel();
+            listBox1 = new ListBox();
             guna2Panel1.SuspendLayout();
+            panel1.SuspendLayout();
             SuspendLayout();
             // 
             // guna2Panel1
@@ -46,10 +48,10 @@
             guna2Panel1.BackColor = Color.SlateBlue;
             guna2Panel1.Controls.Add(guna2ControlBox2);
             guna2Panel1.Controls.Add(guna2ControlBox1);
-            guna2Panel1.CustomizableEdges = customizableEdges5;
+            guna2Panel1.CustomizableEdges = customizableEdges11;
             guna2Panel1.Location = new Point(-5, 0);
             guna2Panel1.Name = "guna2Panel1";
-            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges6;
+            guna2Panel1.ShadowDecoration.CustomizableEdges = customizableEdges12;
             guna2Panel1.Size = new Size(1356, 60);
             guna2Panel1.TabIndex = 2;
             // 
@@ -58,14 +60,14 @@
             guna2ControlBox2.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             guna2ControlBox2.BackColor = Color.Transparent;
             guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox;
-            guna2ControlBox2.CustomizableEdges = customizableEdges1;
+            guna2ControlBox2.CustomizableEdges = customizableEdges7;
             guna2ControlBox2.FillColor = Color.Transparent;
             guna2ControlBox2.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
             guna2ControlBox2.HoverState.FillColor = Color.Red;
             guna2ControlBox2.IconColor = Color.White;
             guna2ControlBox2.Location = new Point(1271, 10);
             guna2ControlBox2.Name = "guna2ControlBox2";
-            guna2ControlBox2.ShadowDecoration.CustomizableEdges = customizableEdges2;
+            guna2ControlBox2.ShadowDecoration.CustomizableEdges = customizableEdges8;
             guna2ControlBox2.Size = new Size(33, 44);
             guna2ControlBox2.TabIndex = 1;
             // 
@@ -74,24 +76,34 @@
             guna2ControlBox1.Anchor = AnchorStyles.Top | AnchorStyles.Right;
             guna2ControlBox1.BackColor = Color.Transparent;
             guna2ControlBox1.CustomIconSize = 40F;
-            guna2ControlBox1.CustomizableEdges = customizableEdges3;
+            guna2ControlBox1.CustomizableEdges = customizableEdges9;
             guna2ControlBox1.FillColor = Color.Transparent;
             guna2ControlBox1.Font = new Font("Segoe UI", 11F, FontStyle.Regular, GraphicsUnit.Point, 0);
             guna2ControlBox1.HoverState.FillColor = Color.Red;
             guna2ControlBox1.IconColor = Color.White;
             guna2ControlBox1.Location = new Point(1306, 9);
             guna2ControlBox1.Name = "guna2ControlBox1";
-            guna2ControlBox1.ShadowDecoration.CustomizableEdges = customizableEdges4;
+            guna2ControlBox1.ShadowDecoration.CustomizableEdges = customizableEdges10;
             guna2ControlBox1.Size = new Size(44, 44);
             guna2ControlBox1.TabIndex = 0;
             // 
-            // flowLayoutPanel1
+            // panel1
             // 
-            flowLayoutPanel1.BackColor = Color.DarkBlue;
-            flowLayoutPanel1.Location = new Point(27, 86);
-            flowLayoutPanel1.Name = "flowLayoutPanel1";
-            flowLayoutPanel1.Size = new Size(1300, 826);
-            flowLayoutPanel1.TabIndex = 3;
+            panel1.BackColor = Color.Lavender;
+            panel1.Controls.Add(listBox1);
+            panel1.Location = new Point(-1, 59);
+            panel1.Name = "panel1";
+            panel1.Size = new Size(329, 871);
+            panel1.TabIndex = 3;
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.ItemHeight = 25;
+            listBox1.Location = new Point(0, 0);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(326, 854);
+            listBox1.TabIndex = 0;
             // 
             // Home_Page
             // 
@@ -100,13 +112,14 @@
             AutoScroll = true;
             BackColor = Color.MidnightBlue;
             ClientSize = new Size(1350, 950);
-            Controls.Add(flowLayoutPanel1);
+            Controls.Add(panel1);
             Controls.Add(guna2Panel1);
             FormBorderStyle = FormBorderStyle.None;
             Name = "Home_Page";
             StartPosition = FormStartPosition.CenterScreen;
             Text = "HOME";
             guna2Panel1.ResumeLayout(false);
+            panel1.ResumeLayout(false);
             ResumeLayout(false);
         }
 
@@ -114,6 +127,7 @@
         private Guna.UI2.WinForms.Guna2Panel guna2Panel1;
         private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox1;
         private Guna.UI2.WinForms.Guna2ControlBox guna2ControlBox2;
-        private FlowLayoutPanel flowLayoutPanel1;
+        private Panel panel1;
+        private ListBox listBox1;
     }
 }
--- Home Page.cs
+++ Home Page.cs
@@ -1,11 +1,8 @@
-﻿using System;
+﻿using Final_Project.Data;
+using Final_Project.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace Final_Project
@@ -15,6 +12,40 @@ namespace Final_Project
         public Home_Page()
         {
             InitializeComponent();
+            this.Load += Home_Page_Load;
+        }
+
+        private async void Home_Page_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                using (var context = new ECommerceContext())
+                {
+                    var products = await context.Products.ToListAsync();
+                    AddProductsToListBox(products);
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"An error occurred: {ex.Message}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void AddProductsToListBox(List<Products> products)
+        {
+            if (listBox1.InvokeRequired)
+            {
+                listBox1.Invoke(new Action(() => AddProductsToListBox(products)));
+            }
+            else
+            {
+                listBox1.Items.Clear();
+
+                foreach (var product in products)
+                {
+                    listBox1.Items.Add($"{product.ProductName}");
+                }
+            }
         }
     }
 }
