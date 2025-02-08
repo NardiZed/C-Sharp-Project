b2eed4ab9f954a447305fd0c19af335c419c6575
Nardos Zerihun <nardoszerihun437@gmail.com>
2025-02-08 21:03:08 +0300

    Made a method with a parameter that takes the name of the category to create the small panels

--- Home Page.cs
+++ Home Page.cs
@@ -20,13 +20,13 @@ namespace Final_Project
 
         }
 
-        private void guna2Button1_Click(object sender, EventArgs e)
+        public void productPanels(String Category)
         {
             using (var context = new ECommerceContext())
             {
-                
+
                 var Electronics = context.Products
-                                   .Where(p => p.Category == "Electronics")
+                                   .Where(p => p.Category == Category)
                                    .ToList();
                 foreach (var product in Electronics)
                 {
@@ -101,83 +101,15 @@ namespace Final_Project
                     flowLayoutPanel1.Controls.Add(name);
                 }
             }
-
+        }
+        private void guna2Button1_Click(object sender, EventArgs e)
+        {
+            productPanels("Electronics");
         }
 
         private void guna2Button2_Click(object sender, EventArgs e)
         {
-            using (var context = new ECommerceContext())
-            {
-                var Electronics = context.Products
-                                       .Where(p => p.Category == "Electronics")
-                                       .ToList();
-
-                foreach (var product in Electronics)
-                {
-                    Panel name = new Panel
-                    {
-                        Size = new Size(240, 390), // Adjust width to fit 4 panels in 1099px width (with margins)
-                        BackColor = Color.Black,
-                        BorderStyle = BorderStyle.FixedSingle,
-                        Margin = new Padding(5) // Add some margin to separate panels
-                    };
-
-                    Label label1 = new Label
-                    {
-                        Text = product.ProductName,
-                        Font = new Font("Serif", 14, FontStyle.Bold),
-                        ForeColor = Color.White,
-                        TextAlign = ContentAlignment.MiddleCenter,
-                        Dock = DockStyle.Top,
-                        Height = 40,
-                    };
-
-                    Label label2 = new Label
-                    {
-                        Text = $"Price: {product.ProductPrice:F2}",
-                        Font = new Font("Serif", 11, FontStyle.Regular),
-                        ForeColor = Color.White,
-                        TextAlign = ContentAlignment.MiddleCenter,
-                        Dock = DockStyle.Top,
-                        Height = 30,
-                    };
-
-                    PictureBox picBox = new PictureBox
-                    {
-                        Image = null,
-                        SizeMode = PictureBoxSizeMode.StretchImage,
-                        Dock = DockStyle.Top,
-                        Height = 200,
-                    };
-
-                    Label label3 = new Label
-                    {
-                        Text = $"Description: {product.ProductDescription}",
-                        Font = new Font("Serif", 10, FontStyle.Italic),
-                        ForeColor = Color.White,
-                        TextAlign = ContentAlignment.MiddleCenter,
-                        Size = new Size(250, 60),
-                        Dock = DockStyle.Top,
-                    };
-
-                    Button addButton = new Button
-                    {
-                        Text = "+",
-                        Font = new Font("Serif", 15),
-                        ForeColor = Color.White,
-                        BackColor = Color.LightGreen,
-                        Height = 40,
-                        Dock = DockStyle.Bottom,
-                    };
-                    name.Controls.Add(addButton);
-                    name.Controls.Add(label3);
-                    name.Controls.Add(picBox);
-                    name.Controls.Add(label2);
-                    name.Controls.Add(label1);
-
-                    flowLayoutPanel1.Controls.Add(name);
-                }
-            }
+            productPanels("Fashion");
         }
     }
 }
